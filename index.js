351c260c836afddb5c7f95108dd7e6cc9face59a
Sean Booker Jr <seanbookerjunior@gmail.com>
2022-06-07 09:02:50 -0400

    commit tree 456b5ee6706ab6baebab5b591dff9bd05c28cb72

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "Sean is the champion";
+
+document.body.appendChild(newHeader);
+
